da76d77c281015e036dd8b924f721a2f7e1adbeb
saiaddanki <saijyothi.addanki@gmail.com>
2022-03-22 20:33:04 +0100

    add verify recieving amounts

--- cypress/integration/go.js
+++ cypress/integration/go.js
@@ -25,4 +25,35 @@ describe("Transfer api tests", function () {
       expect(resp.body.deliveryOptions).to.have.property("standard");
     });
   });
+  it.only("Verify receiving amount is calculated correctly", function () {
+    var eps = [
+      generic.getEP("150.00", "LT", "PL", "EUR", "EUR"),
+      generic.getEP("150.00", "TR", "PL", "TRY", "EUR"),
+    ];
+    for (let i = 0; i < eps.length; i++) {
+      cy.request({
+        url: eps[i],
+        failOnStatusCode: false,
+      }).then((resp) => {
+        var finalFee =
+          resp.body.deliveryOptions.standard.paymentOptions.bank.quote.fees
+            .finalFee;
+        var sendingAmount =
+          resp.body.deliveryOptions.standard.paymentOptions.bank.quote
+            .sendingAmount;
+        var receivingAmount =
+          resp.body.deliveryOptions.standard.paymentOptions.bank.quote
+            .receivingAmount;
+        var rate =
+          resp.body.deliveryOptions.standard.paymentOptions.bank.quote.rate;
+        expect(receivingAmount) ===
+          ((sendingAmount - finalFee) * rate).toFixed(2);
+        cy.log(
+          receivingAmount +
+            "verifies with" +
+            ((sendingAmount - finalFee) * rate).toFixed(2)
+        );
+      });
+    }
+  });
 });
